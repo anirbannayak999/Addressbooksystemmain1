34466e34709e7521585daf48c403cc6721e513e3
anirbannayak999 <anirbannayak999@gmail.com>
2023-01-11 21:34:26 +0530

    welcome to addressbooksystem

---
+++ src/com/bridgelabz/AddressBookmain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class AddressBookmain {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to addressbook");
+    }
+    
+    
+}
