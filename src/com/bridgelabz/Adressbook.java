da7654920d75b5e459af0c9493d0cec7dacb2fde
anirbannayak999 <anirbannayak999@gmail.com>
2023-01-11 22:28:55 +0530

    adressbook

---
+++ src/com/bridgelabz/Adressbook.java
@@ -0,0 +1,40 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Adressbook {
+
+   static Scanner scanner = new Scanner(System.in);
+
+    ArrayList<Contact1> addressbook = new ArrayList<>();
+
+    void addcontactdetail() {
+        Contact1 Contact = new Contact1();
+        System.out.println("enter first name");
+        String firstname= scanner.next();
+        System.out.println("enter last name");
+        String lastname= scanner.next();
+        System.out.println("enter your address");
+        String address= scanner.next();
+        System.out.println("enter your state");
+        String state= scanner.next();
+        System.out.println("enter your city");
+        String city=scanner.next();
+        System.out.println("enter zip code");
+        String zip=scanner.next();
+        System.out.println("enter email address");
+        String email=scanner.next();
+        Contact.setFirstName(firstname);
+        Contact.setLastName(lastname);
+        Contact.setAddress(address);
+        Contact.setState(state);
+        Contact.setCity(city);
+        Contact.setZip(zip);
+        Contact.setEmail(email);
+
+        addressbook.add(Contact);
+
+
+    }
+}
